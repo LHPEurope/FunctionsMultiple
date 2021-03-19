0fd5f16059aa80d059b6c611bcf9b2623b693001
damianodantuonolhp <damiano.dantuono@lhpes.com>
2021-03-19 12:16:20 +0100

    changed message

--- provaJS/index.js
+++ provaJS/index.js
@@ -1,11 +1,5 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+module.exports = async (context, req) => {
+    const responseMessage = "Hello from JS";
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
